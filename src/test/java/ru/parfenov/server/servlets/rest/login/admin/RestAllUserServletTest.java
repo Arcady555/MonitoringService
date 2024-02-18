b5bbf29f7c63dcc6ccaa1f1dc3f07958ca0bdb59
Arcady555 <parfenov7233@gmail.com>
2024-02-18 17:27:28 +0300

    log

---
+++ src/test/java/ru/parfenov/server/servlets/rest/login/admin/RestAllUserServletTest.java
@@ -0,0 +1,12 @@
+package ru.parfenov.server.servlets.rest.login.admin;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RestAllUserServletTest {
+
+    @Test
+    void doGet() {
+    }
+}
