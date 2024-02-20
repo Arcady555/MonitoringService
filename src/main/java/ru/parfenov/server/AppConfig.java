4653270079da9b50b24058f5bc2c002735b46d91
Arcady555 <parfenov7233@gmail.com>
2024-02-20 18:55:15 +0300

    Spring Core - первичная архитектура

---
+++ src/main/java/ru/parfenov/server/AppConfig.java
@@ -0,0 +1,9 @@
+package ru.parfenov.server;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("ru.parfenov")
+public class AppConfig {
+}
